fd051bf5fa0969aea8719110d9d502fd50cc6b00
Matthew Miller <138531020+MatthewMiller523@users.noreply.github.com>
2026-03-30 15:06:56 -0600

    housekeeping and dirtymain

---
+++ dirtymain.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 15:04:51 2026
+
+@author: Matthew Miller
+
+Dirty main
+
+Runs main.py but with more variables in workspace for easier debugging
+"""
+
+from ecg_lib.load_data0 import load_data0
+#import settings
+from pathlib import Path
+import tomllib
+
+#get settings.toml information
+base_dir = Path(__file__).resolve().parent
+settings_path = base_dir / "settings.toml"
+with open(settings_path, "rb") as f:
+    config = tomllib.load(f)
+
+#load data
+self.data, self.truth = load_data0(config)
+
+#split data
+
+#call the training function in train.py
+
