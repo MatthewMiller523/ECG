8fdafe455340769bd403fd1b22a12075091f4e79
Matthew Miller <138531020+MatthewMiller523@users.noreply.github.com>
2026-05-13 13:56:40 -0600

    multiclass dbg

--- main.py
+++ main.py
@@ -12,40 +12,49 @@ N=0.1
 from ecg_lib.read_config_0 import Config
 from ecg_lib.load_data_1 import load_data_0 as ld
 from ecg_lib.fold_func_1 import fold_1 as fold      #old ~fold_func_0 import fold_0 as fold
-from ecg_lib.preprocessing_2 import preprocessing_fun_0 as ppm
-from ecg_lib.train_1 import m_train as mtrain
-from ecg_lib.model_test_0 import m_test_0 as mtest
+from ecg_lib.preprocessing_2 import preprocessing_func_1 as ppm
+from ecg_lib.train_2 import m_train as mtrain
+from ecg_lib.model_test_2 import m_test_1 as mtest
 from ecg_lib.output_write_0 import o_write_0 as o_write
+from ecg_lib.runtime_config import runtime_config_class as rcc
 import sys
 
 
 def main():
 
     cfg = Config()
+    runtime = rcc()
 
     #load data
     data, truth = ld(cfg)
-    fold_data = fold(data, truth, cfg)
-    
+    fold_data, runtime = fold(data, truth, cfg, runtime)
 
-    
-    return fold_data
-    
-    train_loader, val_loader, test_loader = ppm(fold_data, cfg)
+    print("X_train:", len(fold_data["X_train"]))
+    print("X_val:", len(fold_data["X_val"]))
+    print("X_test:", len(fold_data["X_test"]))
+
+    print("Y_train:", len(fold_data["Y_train"]))
+    print("Y_val:", len(fold_data["Y_val"]))
+    print("Y_test:", len(fold_data["Y_test"]))
+
+    print("md_train:", len(fold_data["md_train"]))
+    print("md_val:", len(fold_data["md_val"]))
+    print("md_test:", len(fold_data["md_test"]))
+
+    train_loader, val_loader, test_loader = ppm(fold_data, cfg, runtime)
 
     #train model
-    t_model, status_var = mtrain(train_loader, val_loader, cfg)
+    t_model, status_var = mtrain(train_loader, val_loader, cfg, runtime)
 
     #write training metadata to file
     if cfg.meta['output_csv']:
         o_write(status_var, cfg)
-    
+
     #test model
-    t_loss, t_acc = mtest(test_loader, t_model)
+    t_loss, t_acc_label, t_acc_exact = mtest(test_loader, t_model)
 
 
-
-    return fold_data
+    return t_loss, t_acc_label, t_acc_exact
 
 if __name__== '__main__':
     dbg = main()        #dbg is all debugging (dbg) variables
